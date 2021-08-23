7e70e82ddc7a79621129dcbd52903891a2275e13
zipperdev <sh20101119@gmail.com>
2021-08-23 17:49:51 +0900

    Assignment 11(Continue from nomadcoffee-frontend commit)

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [ loading, setLoading ] = useState(true);
+    const preload = () => {
+        const fonts = [Ionicons.font];
+        const images = [require("./assets/logo.png")];
+        const fontPromise = fonts.map(font => Font.loadAsync(font));
+        const imagePromise = images.map(image => Asset.loadAsync(image));
+        return Promise.all([ ...fontPromise, ...imagePromise ]);
+    };
+    if (loading) {
+        return <AppLoading startAsync={preload} onFinish={() => setLoading(false)} onError={console.warn} />
+    };
+    return (
+        <View style={styles.container}>
+            <Text>Nomad X Coffee Application</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1, 
+        backgroundColor: "#fff", 
+        alignItems: "center", 
+        justifyContent: "center"
+    }
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# Nomad X Coffee
+
+Nomad Coffee Application with React and Apollo Client
